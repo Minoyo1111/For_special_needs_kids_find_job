b949133f14f0fdb14791908a34ae79f1b1e4c09e
mino <oil_chen@cycu.org.tw>
2021-01-16 22:28:42 +0800

    job CRUD finish

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,8 +1,48 @@
 class JobsController < ApplicationController
+  before_action :find_job, only: [:edit, :update, :destroy]
   def index
     @jobs = Job.all
   end
+
   def show
     @job = Job.find(params[:id])
   end
-end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(job_params)
+    if @job.save
+      redirect_to jobs_path, notice: "新增成功"
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @job.update(job_params)
+      redirect_to jobs_path, notice: "更新成功"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @job.destroy if @job
+    redirect_to jobs_path, notice: "職缺已刪除"
+  end
+
+  private
+    def job_params
+      params.require(:job).permit(:title, :store_name, :contact_phone, :area, :description, :lunch, :pay)
+    end
+
+    def find_job
+      @job = Job.find_by(id: params[:id])
+    end
+  end
