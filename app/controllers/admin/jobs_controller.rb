8e079f16398ca5d0a95c195dc9d56e5be8fca74f
mino <oil_chen@cycu.org.tw>
2021-01-17 16:54:14 +0800

    admin/job CRUD

---
+++ app/controllers/admin/jobs_controller.rb
@@ -0,0 +1,47 @@
+class Admin::JobsController < ApplicationController
+  def show
+    @job = Job.find_by(params[:id])
+  end
+
+  def index 
+    @jobs = Job.all
+  end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new(params[:id])
+    if @job.save
+      redirect_to admin_jobs_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @job = Job.find_by(params[:id])
+  end
+
+  def update
+    @job = Job.find_by(params[:id])
+    if @job.update(job_params)
+      redirect_to admin_jobs_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @job = Job.find_by(params[:id])
+    @job.destroy if @job
+    redirect_to admin_jobs_path
+  end
+
+  private
+  def job_params
+    params.require(:job).permit(:title, :store_name, :contact_phone, :area, :description, :lunch, :pay)
+  end
+  
+end
